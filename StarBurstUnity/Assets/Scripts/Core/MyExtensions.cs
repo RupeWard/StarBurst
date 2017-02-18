cebe94c82eefce588c8b206dc3cbd09fbf3e37aa
RupeWard <rupe@rupertward.co.uk>
2017-02-18 17:55:09 +0000

    Button panel tweening

--- StarBurstUnity/Assets/Scripts/Core/MyExtensions.cs
+++ StarBurstUnity/Assets/Scripts/Core/MyExtensions.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public static class MyExtensions
 {
+	static public Color Alphaed( this Color c, float a )
+	{
+		Color result = c;
+		result.a = a;
+		return result;
+	}
+
 	/*
 	static public string DebugDescribe<T>( this T t) where T : IDebugDescribable
 	{
--- StarBurstUnity/Assets/Scripts/Scenes/SceneControllerGame.cs
+++ StarBurstUnity/Assets/Scripts/Scenes/SceneControllerGame.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class SceneControllerGame : SceneController_Base 
 {
@@ -8,12 +9,16 @@ public class SceneControllerGame : SceneController_Base
 	public UnityEngine.UI.Text versionText;
 	public float delay = 2f;
 
-	public GameObject buttonPanel;
+	public float buttonPanelTweenDurn = 0.5f;
+
+	public RectTransform buttonPanelRT;
+	public UnityEngine.UI.Image showButtonPanelImage;
 
 	#endregion inspector hooks
 
 	private void MoveOn()
 	{
+		HideButtonPanel( );
 	}
 
 #region event handlers
@@ -30,12 +35,60 @@ public class SceneControllerGame : SceneController_Base
 	override protected void PostStart()
 	{
 		versionText.text = RJWard.Core.Version.versionNumber.DebugDescribe ();
-
-		buttonPanel.gameObject.SetActive( false );
-
+		
 		Invoke( "MoveOn", delay );
 	}
 
 	#endregion SceneController_Base
 
+	void ShowButtonPanel( )
+	{
+		ShowVersionText( );
+		Sequence sequence = DOTween.Sequence( );
+		showButtonPanelImage.color = showButtonPanelImage.color.Alphaed( 1f );
+		sequence.Append( showButtonPanelImage.DOColor( showButtonPanelImage.color.Alphaed( 0f ), buttonPanelTweenDurn/2f ));
+
+		sequence.Append(buttonPanelRT.DOAnchorPosX( 0f, buttonPanelTweenDurn ).SetEase( Ease.InOutQuad ));
+		sequence.Join(versionText.DOColor( versionText.color.Alphaed(1f), buttonPanelTweenDurn ));
+	}
+
+	void HideButtonPanel( )
+	{
+		Sequence sequence = DOTween.Sequence( );
+		showButtonPanelImage.color = showButtonPanelImage.color.Alphaed( 0f );
+		sequence.Append(buttonPanelRT.DOAnchorPosX( 60f, buttonPanelTweenDurn ).SetEase( Ease.InOutQuad ).OnComplete( HideVersionText));
+		sequence.Append( showButtonPanelImage.DOColor( showButtonPanelImage.color.Alphaed( 1f ), buttonPanelTweenDurn/2f ) );
+
+		versionText.DOColor( versionText.color.Alphaed( 0f ), buttonPanelTweenDurn );
+	}
+
+	public void HandleShowButtonPanelButtonClick( )
+	{
+		ShowButtonPanel( );
+	}
+
+	public void HandleHideButtonPanelButtonClick( )
+	{
+		HideButtonPanel( );
+	}
+
+	public void HandleQuitButtonClick( )
+	{
+		Application.Quit( );
+	}
+
+	private void ShowVersionText( )
+	{
+		ShowVersionText( true );
+	}
+
+	private void HideVersionText( )
+	{
+		ShowVersionText( false);
+	}
+
+	private void ShowVersionText(bool b)
+	{
+		versionText.gameObject.SetActive( b );
+	}
 }
