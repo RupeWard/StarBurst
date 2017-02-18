a6d10932b3e7a0b1e4476181b9e834b1687711dd
RupeWard <rupe@rupertward.co.uk>
2017-02-18 22:01:53 +0000

    Initial Player & GameManager

---
+++ StarBurstUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RJWard.StarBurst
+{
+	public class GameManager: RJWard.Core.Singleton.SingletonSceneLifetime<GameManager>
+	{
+		public GameObject playerPrefab;
+
+		private Player _player;
+
+		void Start( )
+		{
+			_player = Instantiate( playerPrefab ).GetComponent<Player>( );
+			_player.gameObject.name = "Player";
+
+		}
+
+	}
+}
---
+++ StarBurstUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RJWard.StarBurst
+{
+	public class Player : MonoBehaviour
+	{
+		public float speed = 1f;
+
+		private void Update()
+		{
+			if (Input.GetKey(KeyCode.D))
+			{
+				transform.position += transform.forward * speed * Time.deltaTime;
+			}
+			/*
+			if (Input.GetKey( KeyCode.A ))
+			{
+				transform.position -= transform.forward * speed * Time.deltaTime;
+			}
+			*/
+		}
+	}
+
+}
